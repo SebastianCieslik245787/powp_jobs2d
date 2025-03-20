36e6cd36b443c9c5254a8cf9b8cfeb7f10dd9ee2
Sebastian <245787@edu.p.lodz.pl>
2025-03-20 13:45:38 +0100

    Zadanie 2,3 Lab 2

--- src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -7,6 +7,7 @@ public class OperateToCommand implements DriverCommand {
     private Job2dDriver driver;
     private int x;
     private int y;
+
     @Override
     public void execute() {
         driver.operateTo(x, y);
--- src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -7,6 +7,7 @@ public class SetPositionCommand implements DriverCommand {
     private Job2dDriver driver;
     private int x;
     private int y;
+
     @Override
     public void execute() {
         driver.setPosition(x, y);
