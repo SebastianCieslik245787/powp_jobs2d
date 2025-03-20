a397119867d938609a52dc8f864211101cfc7571
Sebastian <245787@edu.p.lodz.pl>
2025-03-20 13:27:05 +0100

    Zadanie 1 Lab 2

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.commands;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class OperateToCommand implements DriverCommand {
+    private Job2dDriver driver;
+    private int x;
+    private int y;
+    @Override
+    public void execute() {
+        driver.operateTo(x, y);
+    }
+
+    public OperateToCommand(Job2dDriver driver, int x, int y) {
+        this.driver = driver;
+        this.x = x;
+        this.y = y;
+    }
+}
